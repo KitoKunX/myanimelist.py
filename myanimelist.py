a2b7da2a99f67e4265b6a03bd013df768c94af57
KitoKunX <KitoKunX@users.noreply.github.com>
2015-09-23 10:16:02 +1000

    Update myanimelist.py

--- myanimelist.py
+++ myanimelist.py
@@ -18,7 +18,7 @@ class MAL(object):
 		- Search Url :: _search_url
 	Functions:
 		- Auth Confirmation :: confirm_auth
-		- Anime/Manga search :: search
+		- Anime/Manga search :: search :: Arguements =  query, type (anime/manga)
 	'''
 	 
 	 def __init__(self):
@@ -31,6 +31,46 @@ class MAL(object):
 		c_auth = requests.get('http://myanimelist.net/api/account/verify_credentials.xml', auth = (self.username, self.password))
 		return c_auth
 		
-	def search(self):
+	def search(self, query, type):
+		data = {"q":args}
+		_search_url = self._search_url
+		_search_url = _search_url % (type)
+		c_obj = requsts.get(
+				_search_url,
+				auth = (self.username, self.password),
+				headers = {},
+				timeout = 30
+		)
+		return
+		#create object for each type
+		
+							
+		
+		
+class Anime(object):
+
+	'''
+	Anime object for MyAnimeList.
+	Required Class:
+		- MAL
+	Given Properties:
+		- Title
+		- Synonyms
+		- Episodes
+		- Synopsis
+		- Air Date
+		- Few other information
+	Functions:
+		- Linked with MAL object
+	'''
+	
+	def __init__(self):
+		self.title = ""
+		self.synonyms = ""
+		self.episodes = ""
+		self.synopsis = ""
+		self.air_data = ""
+		
+	def set_properties(*args):
 		return
 		
